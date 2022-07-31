9ab087fbd78503cfbb54507d6cb2c43dc2067ed2
jjlevinemusic <jjlevinemusic@gmail.com>
2022-07-31 18:01:19 +0300

    Create remove_corr.py

---
+++ remove_corr.py
@@ -0,0 +1,24 @@
+def remove_corr(df,corr_limit=.9):
+  duplicates=[]
+
+  seen=[]
+
+  for i in column_list:
+
+      if i not in seen:
+
+          seen.append(i)
+
+          for j in column_list:
+
+              if j not in seen:
+
+                  if abs(df.fillna(0)[i].corr(df.fillna(0)[j]))>corr_limit:
+
+                      print(i,j,df.fillna(0)[i].corr(df.fillna(0)[j]))
+
+                      duplicates.append(j)
+
+                      seen.append(j)
+return df.drop(duplicates,axis=1)
+
